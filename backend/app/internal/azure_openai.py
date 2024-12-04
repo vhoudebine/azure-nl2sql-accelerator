5999bb68e61356daf37f771cd674fadd378edfbc
Juhyun Lee <juhyunlee@microsoft.com>
2024-12-04 13:32:21 -0500

    streaming response added

--- backend/app/internal/azure_openai.py
+++ backend/app/internal/azure_openai.py
@@ -1,9 +1,16 @@
-from openai import AzureOpenAI
+from openai import AzureOpenAI, AsyncAzureOpenAI
+import time
 
 class AzureOpenAIService:
 
     def __init__(self, endpoint: str, api_key: str, deployment_name: str):
+        # TODO: may need to set up AsyncAzureOpenAI client only and convert all the calls under this class to async
         self.client = AzureOpenAI(
+            azure_endpoint=endpoint,
+            api_key=api_key,
+            api_version="2024-06-01"
+        )
+        self.stream_client = AsyncAzureOpenAI(
             azure_endpoint=endpoint,
             api_key=api_key,
             api_version="2024-06-01"
@@ -11,8 +18,11 @@ class AzureOpenAIService:
         self.deployment_name = deployment_name
         self.system_prompt = "You Are helpful assistant."
 
+        
+
     def close(self):
         self.client.close()
+        self.stream_client.close()
     
     def aoai_completion_simple(self, user_prompt: str) -> str:
         # checking if the user prompt is empty string, if it is throw an error
@@ -38,3 +48,32 @@ class AzureOpenAIService:
 
         return response.choices[0].message.content
 
+    async def aoai_completion_simple_stream(self, user_prompt: str):
+        # checking if the user prompt is empty string, if it is throw an error
+        if user_prompt == "":
+            raise ValueError("User prompt cannot be empty")
+
+        messages = [
+            {
+                "role": "system",
+                "content": self.system_prompt
+            },
+            {
+                "role": "user",
+                "content": user_prompt
+            }
+        ]
+
+        response = await self.stream_client.chat.completions.create(
+            model=self.deployment_name,
+            messages=messages,
+            temperature=0,
+            stream=True
+        )
+
+        async for chunk in response:
+            if len(chunk.choices) > 0:
+                delta = chunk.choices[0].delta
+                if delta.content:
+                    yield delta.content
+         
--- backend/app/routers/v1/test.py
+++ backend/app/routers/v1/test.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Request
+from fastapi.responses import StreamingResponse
 
 router = APIRouter(
     prefix="/test",
@@ -18,11 +19,16 @@ def post_test(obj: dict):
     return {"message": f"POST test with data {obj}"}
 
 @router.post("/aoai")
-async def post_test_aoai(request: Request):
-    print(request)
-    # testing
-    req = await request.json()
-    user_prompt = req.get("user_prompt")
+async def post_test_aoai(obj: dict, request: Request):
+    user_prompt = obj["user_prompt"]
     aoai_client = request.app.state.aoai_client
     resp = aoai_client.aoai_completion_simple(user_prompt)
     return {"message": resp}
+
+@router.post("/aoai-stream")
+async def post_test_aoai(obj: dict, request: Request):
+    # return {"message": "Not implemented yet"}
+    user_prompt = obj["user_prompt"]
+    aoai_client = request.app.state.aoai_client
+    return StreamingResponse(aoai_client.aoai_completion_simple_stream(user_prompt), media_type="text/event-stream")
+    
